8fbeed9c8a3cdf757b22fd303144198a97ab9579
Sultankhan Pathan <89242322+Sul1871p@users.noreply.github.com>
2026-01-31 13:59:05 +0530

    company name changed

--- src/branding.ts
+++ src/branding.ts
@@ -1,6 +1,6 @@
 export const BRANDING = {
   appName: 'FYNTRIX',
-  owner: 'Tradesurf Ventures Private Limited',
+  owner: 'Tradesurf Research Private Limited',
   licensee: 'Mahesh',
   appId: 'FYNTRIX-01',
   signature: 'FYNTRIX-5d4e9f9a-92a1-4e2c-8bb2-3b1d9f4af123',
